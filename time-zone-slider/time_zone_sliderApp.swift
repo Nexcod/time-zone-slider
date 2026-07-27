aee50978a6e24d67e8cddd48733f248f51953e0b
Danil Khismatulin <nexcod@gmail.com>
2026-07-27 14:59:18 +0700

    Initial Commit

---
+++ time-zone-slider/time_zone_sliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_zone_sliderApp.swift
+//  time-zone-slider
+//
+//  Created by Danil Khismatulin on 27.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct time_zone_sliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
